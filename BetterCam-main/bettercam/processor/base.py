63da0af6f5a7829a8fa85de309f3d9c3cc767983
ZCban <100644497+ZCban@users.noreply.github.com>
2024-05-01 16:52:11 +0200

    Update base.py

--- BetterCam-main/bettercam/processor/base.py
+++ BetterCam-main/bettercam/processor/base.py
@@ -5,17 +5,32 @@ class ProcessorBackends(Enum):
     CUPY = 'cupy'
 
 class Processor:
-    def __init__(self, backend=ProcessorBackends.NUMPY, output_color: str = "RGB", nvidia_gpu: bool = False, rotation_angle: int = 0):
+    def __init__(self, backend=ProcessorBackends.CUPY, output_color: str = "RGB", nvidia_gpu: bool = False, rotation_angle: int = 0):
         self.color_mode = output_color
         # Automatically select CuPy if NVIDIA GPU is enabled, else use the specified backend
         selected_backend = ProcessorBackends.CUPY if nvidia_gpu else backend
         self.backend = self._initialize_backend(selected_backend)
-        self.set_rotation_function(rotation_angle)  # Select the processing function based on rotation angle
+        if selected_backend == ProcessorBackends.CUPY:
+            self.set_rotation_CPfunction(rotation_angle)
+        else:
+            self.set_rotation_function(rotation_angle)
 
     def set_rotation_function(self, angle):
-        # Map the angle to the processing function
+        # Map the angle to the processing function in self.backend
         angle_to_function = {
             0: self.backend.processA0,
+            90: self.backend.processA90,
+            180: self.backend.processA180,
+            270: self.backend.processA270
+        }
+        self.process = angle_to_function.get(angle)
+        if not self.process:
+            raise ValueError(f"Unsupported rotation angle: {angle}")
+
+    def set_rotation_CPfunction(self, angle):
+        # Map the angle to the processing function in self.backend (assuming this method is for a GPU context)
+        angle_to_function = {
+            0: self.backend.processCPA0,
             90: self.backend.processA90,
             180: self.backend.processA180,
             270: self.backend.processA270
@@ -29,7 +44,7 @@ class Processor:
         pass
 
     def _initialize_backend(self, backend):
-        # Initialize the appropriate backend based on the ProcessorBackends enum
+        print(f"Initializing backend: {backend}")
         if backend == ProcessorBackends.NUMPY:
             from bettercam.processor.numpy_processor import NumpyProcessor
             return NumpyProcessor(self.color_mode)
@@ -39,5 +54,7 @@ class Processor:
         else:
             raise ValueError(f"Unknown backend: {backend}")
 
-#####USAGE
-#processor = Processor(output_color="RGB", nvidia_gpu=True, rotation_angle=90)
+# Example Usage
+processor = Processor(output_color="RGB", nvidia_gpu=True, rotation_angle=90)
+
+
